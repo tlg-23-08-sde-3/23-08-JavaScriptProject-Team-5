260b1874bff85bd321d6d4a01e84f06c946f498f
Jojo George Mattam <jojogeorge.mattam@tlgcohort.com>
2023-10-16 13:55:47 -0500

    Signup form CSS and Script

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// Function to toggle between signup and signin forms
+
+function toggleForm() {
+  const signInBox = document.querySelector(".signinBox");
+  const signUpBox = document.querySelector(".signupBox");
+
+  if (signInBox.style.display === "block") {
+    signInBox.style.display = "none";
+    signUpBox.style.display = "block";
+  } else {
+    signInBox.style.display = "block";
+    signUpBox.style.display = "none";
+  }
+}
+
+// Attach the toggleForm function to both signup and signin links
+document.querySelector(".signupLink").addEventListener("click", toggleForm);
+document.querySelector(".signinLink").addEventListener("click", toggleForm);
