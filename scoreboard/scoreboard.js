dbe9de3f60fe21ad3ef303bec6be7fd24eb8c05d
Alberto Leon <alberto.leon@icloud.com>
2023-10-16 13:55:04 -0500

    added comments

--- scoreboard/scoreboard.js
+++ scoreboard/scoreboard.js
@@ -1,19 +1,44 @@
+// This event listener ensures that the JavaScript code only runs after the entire HTML document (DOM) has been fully loaded.
 document.addEventListener("DOMContentLoaded", function () {
+    // Array of score objects. Each object represents a player's score, the difficulty level they played at, and the time they took.
     const scores = [
-        { player: "Gen", score: 340 },
-        { player: "Chris", score: 900 },
-        { player: "Joe", score: 120 },
+        { player: "Gen", score: 340, difficulty: "medium", time: "2m 30s" },
+        { player: "Chris", score: 900, difficulty: "hard", time: "3m 15s" },
+        { player: "Joe", score: 120, difficulty: "easy", time: "1m 45s" },
+        { player: "Gen", score: 340, difficulty: "medium", time: "2m 30s" },
+        { player: "Chris", score: 900, difficulty: "hard", time: "3m 15s" },
+        { player: "Joe", score: 120, difficulty: "easy", time: "1m 45s" },
+        { player: "Gen", score: 340, difficulty: "medium", time: "2m 30s" },
+        { player: "Chris", score: 900, difficulty: "hard", time: "3m 15s" },
+        { player: "Joe", score: 120, difficulty: "easy", time: "1m 45s" },
+        { player: "Gen", score: 340, difficulty: "medium", time: "2m 30s" },
+        { player: "Chris", score: 900, difficulty: "hard", time: "3m 15s" },
+        { player: "Joe", score: 120, difficulty: "easy", time: "1m 45s" },
     ];
 
+    // Sort the scores array in descending order based on the score value.
+    // This ensures that the highest scores are displayed at the top of the scoreboard.
     scores.sort((a, b) => b.score - a.score);
 
+    // Get a reference to the table body element in the HTML where the scores will be appended.
+    const scoreboardBody = document.getElementById("scoreboardBody");
+
+    // Loop through each score object in the sorted scores array.
     scores.forEach((score, index) => {
+        // Create a new table row (tr) element for each score.
         const row = document.createElement("tr");
+
+        // Populate the row with the data from the current score object.
+        // This includes the rank (index + 1), player name, score value, difficulty level, and time.
         row.innerHTML = `
             <th scope="row">${index + 1}</th>
             <td>${score.player}</td>
             <td>${score.score}</td>
+            <td>${score.difficulty}</td>
+            <td>${score.time}</td>
         `;
+
+        // Append the populated row to the table body in the HTML.
         scoreboardBody.appendChild(row);
     });
 });
