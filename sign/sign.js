e2f42f7da37f2d4f26a3d27dc17e6dea7f84ab26
Alberto Leon <alberto.leon@icloud.com>
2023-10-18 15:58:26 -0500

    finished integration of the sign in/up page with the backend

--- sign/sign.js
+++ sign/sign.js
@@ -1,7 +1,7 @@
 // Function to toggle between signup and signin forms
 function toggleForm(event) {
     event.preventDefault();
-    console.log("toggleForm called");
+
     const signInBox = document.querySelector(".signinBox");
     const signUpBox = document.querySelector(".signupBox");
 
@@ -12,16 +12,93 @@ function toggleForm(event) {
         signInBox.style.display = "block";
         signUpBox.style.display = "none";
     }
+
+    // Clear the form fields
+    document.querySelector(".signinBox form").reset();
+    document.querySelector(".signupBox form").reset();
+
+    // Clear any existing messages
+    document.getElementById("signinMessage").textContent = "";
+    document.getElementById("signupMessage").textContent = "";
 }
 
 // Function to handle successful login or signup
 function handleSuccessfulAuth() {
-    console.log("login successful");
     // Hide the sign-in/sign-up section
-    document.querySelector("section").style.display = "none";
+    document.querySelector(".sign-section").style.display = "none";
 
     // Show the game menu
     document.querySelector(".game_menu").style.display = "flex";
+}
+
+// Function to handle user registration
+async function handleUserRegistration(event) {
+    event.preventDefault();
+
+    const username = event.target.username.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    try {
+        const response = await fetch("/api/users/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, email, password }),
+        });
+
+        const data = await response.json();
+
+        if (response.status === 201) {
+            document.getElementById("signupMessage").textContent = data.message;
+            document.getElementById("signupMessage").classList.add("success");
+            setTimeout(() => {
+                toggleForm(event);
+            }, 1750);
+        } else {
+            document.getElementById("signupMessage").textContent = data.message;
+        }
+    } catch (error) {
+        document.getElementById("signupMessage").textContent =
+            "An error occurred during registration.";
+    }
+    event.target.reset();
+}
+
+// Function to handle user login
+async function handleUserLogin(event) {
+    event.preventDefault();
+
+    const username = event.target.username.value;
+    const password = event.target.password.value;
+
+    try {
+        const response = await fetch("/api/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        const data = await response.json();
+
+        if (response.status === 200) {
+            document.getElementById("signinMessage").textContent =
+                "Logged in successfully.";
+            document.getElementById("signinMessage").classList.add("success");
+            setTimeout(() => {
+                handleSuccessfulAuth();
+            }, 1750);
+        } else {
+            document.getElementById("signinMessage").textContent = data.message;
+        }
+    } catch (error) {
+        document.getElementById("signinMessage").textContent =
+            "An error occurred during login.";
+    }
+    event.target.reset();
 }
 
 // Attach the toggleForm function to both signup and signin links
@@ -32,10 +109,10 @@ document
     .querySelector(".signupBox .signup a")
     .addEventListener("click", toggleForm);
 
-// Attach the handleSuccessfulAuth function to the login and signup buttons
+// Attach the handleUserRegistration and handleUserLogin functions to the signup and login forms
 document
-    .querySelector("input[name='login']")
-    .addEventListener("click", handleSuccessfulAuth);
+    .querySelector(".signupBox form")
+    .addEventListener("submit", handleUserRegistration);
 document
-    .querySelector("input[name='signup']")
-    .addEventListener("click", handleSuccessfulAuth);
+    .querySelector(".signinBox form")
+    .addEventListener("submit", handleUserLogin);
