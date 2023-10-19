ed66b0b70aa318e6fd7f6eb44088ee77ef6554f3
Alberto Leon <alberto.leon@icloud.com>
2023-10-19 11:59:19 -0500

    refactored menu.js

--- menu/menu.js
+++ menu/menu.js
@@ -1,16 +1,5 @@
 // Animation for the buttons
-TweenMax.staggerFrom(
-    ".btn",
-    2,
-    {
-        scale: 0.5,
-        opacity: 0,
-        delay: 0.5,
-        ease: Elastic.easeOut,
-        force3D: true,
-    },
-    0.2
-);
+animateButtons();
 
 // Add a click event listener to the "New Game" button
 const newGameButton = document.getElementById("new-game");
@@ -88,6 +77,135 @@ difficultyButtons.addEventListener("click", function (event) {
     }
 });
 
+// Add a click event listener to the "Credits" button
+const creditsButton = document.getElementById("credits-button");
+creditsButton.addEventListener("click", function () {
+    // Hide the main menu
+    const mainMenu = document.querySelector(".game_menu");
+    mainMenu.style.display = "none";
+
+    // Show the credits
+    showCredits();
+});
+
+function animateButtons() {
+    TweenMax.staggerFrom(
+        ".btn",
+        2,
+        {
+            scale: 0.5,
+            opacity: 0,
+            delay: 0.5,
+            ease: Elastic.easeOut,
+            force3D: true,
+        },
+        0.2
+    );
+}
+
+async function populateScoreboard() {
+    try {
+        const scores = await fetchScores();
+        clearScoreboard();
+        appendScoresToScoreboard(scores);
+    } catch (error) {
+        console.error("Error populating the scoreboard:", error);
+    }
+}
+
+async function fetchScores() {
+    const response = await fetch("/api/scores/all");
+    if (!response.ok) {
+        throw new Error("Failed to fetch scores");
+    }
+    return await response.json();
+}
+
+function clearScoreboard() {
+    const scoreboardBody = document.getElementById("scoreboardBody");
+    while (scoreboardBody.firstChild) {
+        scoreboardBody.removeChild(scoreboardBody.firstChild);
+    }
+}
+
+function appendScoresToScoreboard(scores) {
+    const scoreboardBody = document.getElementById("scoreboardBody");
+    scores.forEach((score, index) => {
+        const row = createScoreRow(score, index);
+        scoreboardBody.appendChild(row);
+    });
+}
+
+function createScoreRow(score, index) {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <th scope="row">${index + 1}</th>
+        <td>${score.username}</td>
+        <td>${score.score}</td>
+        <td>${score.difficulty}</td>
+        <td>${score.time}</td>
+    `;
+    return row;
+}
+
+function showCredits() {
+    const creditsMain = createCreditsContainer();
+    document.body.appendChild(creditsMain);
+}
+
+function createCreditsContainer() {
+    const creditsMain = createElement("div", ["credits-container"]);
+    const creditsContainer = createElement("section", ["credits-section"]);
+    const creditsTitle = createElement("h1", ["credits-title"], "CREDITS");
+    const developersList = createDevelopersList([
+        "Alberto Leon",
+        "Jojo George Mattam",
+    ]);
+    const backButton = createCreditsBackButton(creditsMain);
+
+    creditsContainer.appendChild(creditsTitle);
+    creditsContainer.appendChild(developersList);
+    creditsContainer.appendChild(backButton);
+    creditsMain.appendChild(creditsContainer);
+
+    return creditsMain;
+}
+
+function createDevelopersList(developers) {
+    const developersList = createElement("ul", ["developers-list"]);
+    developers.forEach((developer) => {
+        const developerItem = createElement("li", [], developer);
+        developersList.appendChild(developerItem);
+    });
+    return developersList;
+}
+
+function createCreditsBackButton(creditsMain) {
+    const backButton = createElement(
+        "button",
+        ["credits-button"],
+        "Back to Menu"
+    );
+    backButton.addEventListener("click", function () {
+        creditsMain.remove();
+        toggleDisplay(".game_menu", "flex");
+    });
+    return backButton;
+}
+
+function toggleDisplay(selector, displayValue) {
+    document.querySelector(selector).style.display = displayValue;
+}
+
+function createElement(tag, classes = [], textContent = "") {
+    const element = document.createElement(tag);
+    element.classList.add(...classes);
+    if (textContent) {
+        element.textContent = textContent;
+    }
+    return element;
+}
+
 // Create a function to render the scoreboard
 function renderScoreboard() {
     // Create a container element for the scoreboard
@@ -152,102 +270,4 @@ function renderScoreboard() {
     // Append the scoreboard container to the body of the document
     document.body.appendChild(scoreboardMain);
     populateScoreboard();
-}
-
-async function populateScoreboard() {
-    try {
-        // Fetch scores from the backend
-        const response = await fetch("/api/scores/all");
-        if (!response.ok) {
-            throw new Error("Failed to fetch scores");
-        }
-
-        const scores = await response.json();
-
-        // Get a reference to the table body element in the HTML where the scores will be appended.
-        const scoreboardBody = document.getElementById("scoreboardBody");
-
-        // Clear any existing rows in the scoreboard
-        while (scoreboardBody.firstChild) {
-            scoreboardBody.removeChild(scoreboardBody.firstChild);
-        }
-
-        // Loop through each score object in the fetched scores array.
-        scores.forEach((score, index) => {
-            // Create a new table row (tr) element for each score.
-            const row = document.createElement("tr");
-
-            // Populate the row with the data from the current score object.
-            // This includes the rank (index + 1), player name, score value, difficulty level, and time.
-            row.innerHTML = `
-            <th scope="row">${index + 1}</th>
-            <td>${score.username}</td>
-            <td>${score.score}</td>
-            <td>${score.difficulty}</td>
-            <td>${score.time}</td>
-        `;
-
-            // Append the populated row to the table body in the HTML.
-            scoreboardBody.appendChild(row);
-        });
-    } catch (error) {
-        console.error("Error populating the scoreboard:", error);
-    }
-}
-
-// Add a click event listener to the "Credits" button
-const creditsButton = document.getElementById("credits-button");
-creditsButton.addEventListener("click", function () {
-    // Hide the main menu
-    const mainMenu = document.querySelector(".game_menu");
-    mainMenu.style.display = "none";
-
-    // Show the credits
-    showCredits();
-});
-
-function showCredits() {
-    // Create a container element for the credits
-    const creditsMain = document.createElement("div");
-    creditsMain.classList.add("credits-container");
-
-    const creditsContainer = document.createElement("section");
-    creditsContainer.classList.add("credits-section");
-
-    // Create the credits title
-    const creditsTitle = document.createElement("h1");
-    creditsTitle.classList.add("credits-title");
-    creditsTitle.textContent = "CREDITS";
-
-    // Create the list of developers
-    const developersList = document.createElement("ul");
-    developersList.classList.add("developers-list");
-
-    const developers = ["Alberto Leon", "Jojo George Mattam"];
-
-    // Add each developer to the list
-    developers.forEach(function (developer) {
-        const developerItem = document.createElement("li");
-        developerItem.textContent = developer;
-        developersList.appendChild(developerItem);
-    });
-
-    // Create a "Back to Menu" button
-    const backButton = document.createElement("button");
-    backButton.classList.add("credits-button");
-    backButton.textContent = "Back to Menu";
-    backButton.addEventListener("click", function () {
-        // Remove the credits and show the main menu
-        creditsMain.remove();
-        document.querySelector(".game_menu").style.display = "flex";
-    });
-
-    // Append everything together
-    creditsContainer.appendChild(creditsTitle);
-    creditsContainer.appendChild(developersList);
-    creditsContainer.appendChild(backButton);
-    creditsMain.appendChild(creditsContainer);
-
-    // Append the credits container to the body of the document
-    document.body.appendChild(creditsMain);
 }
